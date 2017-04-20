be9193c24f177cd7214bbc5e1898ac5abda5015e
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2017-04-20 17:56:39 +0200

    Adjusted testimony processing to copy all table entries.
    get_testimonies.py will now write all columns from the excel table to
    xslt/testimony-table.xml, thus enabling to work with fields that are not
    displayed in the table.
    
    In order to reduce the amount of data, the XML file will only contain
    elements for fields that have values for the corresponding testimony.
    Additionaly, the XML file contains a header with an entry for each
    column of the original Excel table.
    
    To reduce errors from strange table headers, each table header from the
    excel table is 'slugified':
    
    * unicode-normalized
    * converted to lower case
    * äöüß expanded
    * stretches of non-identifier-chars replaced with -
    * leading/trailing - stripped
    
    So " Biedermann-HerwigNr." becomes "biedermann-herwignr" and
    "Datum.(von)" becomes "datum-von". This is also reflected in the XML
    header.
    
    The XSLT stylesheet now contains the config for the columns and column
    labels to be used, so there is hopefully a single place for everything
    related to representation.

--- get_testimonies.py
+++ get_testimonies.py
@@ -13,23 +13,25 @@ Attributes:
 
 """
 
-selected_columns = ['Gräf-Nr.', 'Pniower-Nr.',  'QuZ', ' Biedermann-HerwigNr.',
-                    'Datum.(von)', 'Dokumenttyp', 'Verfasser', 'Adressat', 'Druckort']
-column_labels = ['Gräf', 'Pniower', 'QuZ', 'Bie3',
-                 'Datum', 'Quellengattung', 'Verfasser', 'Adressat', 'Druckort']
-
-
 import pandas as pd
-from lxml import html, etree
+from lxml import etree
 import requests
 from getpass import getpass
 import io
 import sys
+import unicodedata
+import re
 
+def sanitize_colname(colname):
+    colname = unicodedata.normalize('NFKC', colname).lower()
+    colname = colname.translate({ ord('ä'): 'ae', ord('ü'): 'ue', ord('ö'):
+                                 'oe', ord('ß'): 'ss'})
+    colname = re.sub(r'[\W_-]+', '-', colname).strip('-')
+    return colname
 
 def to_id(row):
-    for column, prefix in [('Gräf', 'graef'), ('Pniower', 'pniower'),
-                           ('QuZ', 'quz'), ('Bie3', 'bie3')]:
+    for column, prefix in [('graef-nr', 'graef'), ('pniower-nr', 'pniower'),
+                           ('quz', 'quz'), ('biedermann-herwignr', 'bie3')]:
         if not pd.isnull(row[column]):
             return prefix + '_' + str(row[column])
 
@@ -60,7 +62,7 @@ def fetch_table():
 
 def read_testimonies(buf, **kwargs):
     """
-    Reads the table from the given object and filters the interesting columns.
+    Reads the table from the given object
 
     Args:
         buf: cf. :func:`pd.read_excel`
@@ -69,88 +71,51 @@ def read_testimonies(buf, **kwargs):
     Returns:
         pd.DataFrame
     """
-    raw_testimonies = pd.read_excel(buf, **kwargs)
-    testimonies = raw_testimonies[selected_columns]
-    testimonies.columns = pd.Index(column_labels)
-    testimonies.loc[:,'Datum'] = testimonies.loc[:,'Datum'].str.replace('00\.', '')
+    return pd.read_excel(buf, **kwargs)
+
+def improve_testimony_table(testimonies):
+    testimonies = testimonies.copy()
+    testimonies.columns = [ sanitize_colname(col) for col in testimonies.columns ]
+
+    for col in testimonies.columns:
+        if col.startswith('datum'):
+            testimonies.loc[:,col] = testimonies.loc[:,col].str.replace('00\.', '')
     testimonies.loc[:,'ID'] = testimonies.apply(to_id, axis=1)
     return testimonies
 
-def to_xml(testimony_df):
+def to_xml(testimony_df, orignames):
     ns = "http://www.faustedition.net/ns"
     NS = "{"+ns+"}"
     nsmap = {None : ns}
     root = etree.Element(NS + "testimonies", nsmap=nsmap)
     root.addprevious(etree.Comment("This document is sporadically re-generated from an Excel sheet in the wiki. Please don't edit it directly"))
+
+    header = root.makeelement(NS + "header")
+    header.append(etree.Comment("These fields have been found in the excel table:"))
+    for col, orig in zip(testimony_df.columns, orignames):
+        if col != 'ID':
+            el = header.makeelement(NS + "fieldspec",
+                                    attrib=dict(name=col, spreadsheet=orig))
+            header.append(el)
+    root.append(header)
+
     for __, row in testimony_df.iterrows():
         row_el = root.makeelement(NS + "testimony")
         if not pd.isnull(row.ID):
             row_el.attrib['id'] = row.ID
         for label, value in row.iteritems():
-            if label is 'ID':
+            if label is 'ID':    # -> attribute
                 continue
-            el = row_el.makeelement(NS + 'field', attrib=dict(label=label))
-            if not pd.isnull(value):
-                el.text = str(value)
+            if pd.isnull(value) or value == '':        # skip empty fields for brevity
+                continue
+            if isinstance(value, float) and int(value) == value:
+                value = int(value)    # there's no nan in int
+            el = row_el.makeelement(NS + 'field', attrib=dict(name=label))
+            el.text = str(value)
             row_el.append(el)
         root.append(row_el)
     return etree.ElementTree(root)
 
-
-def html_table(testimony_df):
-    """
-    Converts the dataframe to an HTML table, and adds appropriate attributes.
-    """
-    table = html.fromstring(testimony_df.to_html(na_rep='', index=False))
-
-    table.attrib['data-sortable'] = 'true'
-    table.attrib['class'] = 'pure-table'
-    headerrow = table.find('thead').find('tr')
-    del headerrow.attrib['style']
-    ths = headerrow.findall('th')
-    for th in ths:
-        th.attrib['data-sorted'] = 'false'
-        if th.text in ['Gräf', 'Pniower']:
-            th.attrib['data-sortable-type'] = 'numericplus'
-        elif th.text == 'Datum':
-            th.attrib['data-sortable-type'] = 'date-de'
-        elif th.text == 'Druckort':
-            th.attrib['data-sortable-type'] = 'bibliography'
-        else:
-            th.attrib['data-sortable-type'] = 'alpha'
-    return table
-
-def test():
-    table = html_table(read_testimonies('Dokumente_zur_Entstehungsgeschichte.xls'))
-    print(html.tostring(table, encoding="unicode"))
-
-
-def write_html(output, table):
-    prefix = """
-<?php $showFooter = false; ?>
-<?php /* ATTENTION: This file is generated by get_testimonies.py. DO NOT EDIT HERE */ ?>
-<?php include "includes/header.php"; ?>
-<section>
-
-  <article>
-      <div id="testimony-table-container">
-"""
-    suffix = """
-      </div>
-  </article>
-
-</section>
-<script type="text/javascript">
-  // set breadcrumbs
-  document.getElementById("breadcrumbs").appendChild(Faust.createBreadcrumbs([{caption: "Archiv", link: "archive"}, {caption: "Dokumente zur Entstehungsgeschichte"}]));
-</script>
-
-<?php include "includes/footer.php"; ?>
-"""
-    with open(output, mode="wt", encoding="utf-8") as out:
-        out.write(prefix)
-        out.write(html.tostring(table, encoding="unicode"))
-        out.write(suffix)
 
 def main():
     pd.set_option('max_colwidth', 10000)
@@ -159,7 +124,8 @@ def main():
     else:
         df = read_testimonies(fetch_table())
     # write_html("src/main/web/archive_testimonies.php", html_table(df))
-    xml = to_xml(df)
+    sanitized = improve_testimony_table(df)
+    xml = to_xml(sanitized, df.columns)
     xml.write('xslt/testimony-table.xml',
               encoding='utf-8',
               xml_declaration=True,
